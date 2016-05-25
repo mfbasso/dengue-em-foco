c66aa80facf62a532e9574f48ba5287dac41ca7e
Renan Luiz Vendramini <rlvendramini@outlook.com>
2016-05-25 15:00:55 -0300

    funções novas de login para o controle Pages

--- application/controllers/Pages.php
+++ application/controllers/Pages.php
@@ -6,15 +6,38 @@ class Pages extends CI_Controller {
 	{
 	        if ( ! file_exists(APPPATH.'views/pages/'.$page.'.php'))
 	        {
-	                // Whoops, we don't have a page for that!
-	                show_404();
+             // Whoops, we don't have a page for that!
+             show_404();
 	        }
 
 	        $data['title'] = ucfirst($page); // Capitalize the first letter
 
-	        //$this->load->view('templates/header', $data);
-	        $this->load->view('pages/'.$page, $data);
-	       //$this->load->view('templates/footer', $data);
+	      	//$this->load->view('templates/header', $data);
+	      	$this->load->view('pages/'.$page, $data);
+	      	//$this->load->view('templates/footer', $data);
+	}
+
+	public function login($page = 'validar')
+	{
+		if($page == 'validar')
+		{
+			if($_POST)
+				$data['userName'] = $_POST['userName'];
+				$data['userEmail'] = $_POST['userEmail'];
+				$data['userPhoto'] = $_POST['userPhoto'];
+	  		$this->load->model('oauth/login');
+	  		$this->login->validar($data['userName'], $data['userEmail'], $data['userPhoto']);
+	  	}
+	  	else if($page == 'redirecionar')
+	  	{
+	  		if($_SESSION)
+			{
+				if($this->session->has_userdata('logged_in'))
+					redirect(site_url().'/pages/view/mapa');
+			}
+		}
+		else
+			show_404();
 	}
 }
 ?>
