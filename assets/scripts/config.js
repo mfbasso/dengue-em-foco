22422bc78c625ecc9cd9a3e152e1565e7a923cf7
Isac Fadoni <isacfadoni@gmail.com>
2016-05-28 18:28:07 -0300

    Scripts - Refatoração do código.

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -1,98 +1,94 @@
 
-//Google Login
-function onSignIn(googleUser)
-	{
+  //Google Login
+  function onSignIn(googleUser) {
 	  var profile = googleUser.getBasicProfile();
 	  localStorage.setItem('userName', profile.getName());
 	  localStorage.setItem('userPhoto', profile.getImageUrl());
-	  localStorage.setItem('userEmail', profile.getEmail());	  
+	  localStorage.setItem('userEmail', profile.getEmail());
 	}
 
-	function signOut()
-	{
+	function signOut() {
 		var auth2 = gapi.auth2.getAuthInstance();
 		auth2.disconnect().then(function () {
 		  console.log('User signed out.');
 		});
-						
-			
 	}
 
 	//Facebook login
 
-		 // This function is called when someone finishes with the Login
+	// This function is called when someone finishes with the Login
   // Button.  See the onlogin handler attached to it in the sample
   // code below.
-  function checkLoginState() {
+  function checkLoginState () {
     FB.login(function(response) {
-        if (response.authResponse) {
-            // usuario aceitou o app
-			testAPI();
-        }
+      if (response.authResponse) {
+        // usuario aceitou o app
+		    testAPI();
+      }
     }, {
-        scope: 'email, publish_actions,user_photos'
+      scope: 'email, publish_actions,user_photos'
     });
-
   }
 
-  window.fbAsyncInit = function()
-  {
+  window.fbAsyncInit = function () {
 	  FB.init({
-		appId      : '803156286438464',
-		cookie     : true,  // enable cookies to allow the server to access 
-							// the session
-		xfbml      : true,  // parse social plugins on this page
-		version    : 'v2.2' // use version 2.2
+  		appId      : '803156286438464',
+  		cookie     : true,  // enable cookies to allow the server to access
+  							          // the session
+  		xfbml      : true,  // parse social plugins on this page
+  		version    : 'v2.2' // use version 2.2
 	  });
 
-  // Now that we've initialized the JavaScript SDK, we call 
-  // FB.getLoginStatus().  This function gets the state of the
-  // person visiting this page and can return one of three states to
-  // the callback you provide.  They can be:
-  //
-  // 1. Logged into your app ('connected')
-  // 2. Logged into Facebook, but not your app ('not_authorized')
-  // 3. Not logged into Facebook and can't tell if they are logged into
-  //    your app or not.
-  //
-  // These three cases are handled in the callback function.
-		FB.getLoginStatus(function(response) {
-		statusChangeCallback(response);
-		});
+    // Now that we've initialized the JavaScript SDK, we call
+    // FB.getLoginStatus().  This function gets the state of the
+    // person visiting this page and can return one of three states to
+    // the callback you provide.  They can be:
+    //
+    // 1. Logged into your app ('connected')
+    // 2. Logged into Facebook, but not your app ('not_authorized')
+    // 3. Not logged into Facebook and can't tell if they are logged into
+    //    your app or not.
+    //
+    // These three cases are handled in the callback function.
+  	FB.getLoginStatus(function(response) {
+  	  statusChangeCallback(response);
+  	});
 
   };
-	// Load the SDK asynchronously
-	  (function(d, s, id)
-	  {
-	    var js, fjs = d.getElementsByTagName(s)[0];
-	    if (d.getElementById(id)) return;
-	    js = d.createElement(s); js.id = id;
-	    js.src = "//connect.facebook.net/en_US/sdk.js";
-	    fjs.parentNode.insertBefore(js, fjs);
-	  }
+
+  // Load the SDK asynchronously
+  (function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = "//connect.facebook.net/en_US/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+  }
   (document, 'script', 'facebook-jssdk'));
+
   // Here we run a very simple test of the Graph API after login is
   // successful.  See statusChangeCallback() for when this call is made.
   function testAPI() {
     console.log('Welcome!  Fetching your information.... ');
     FB.api('/me', function(response) {
-		//Captura informações do PHP
-         $.ajax({
-           type: 'POST',
-           url: "http://localhost/dengue-em-foco/index.php/pages/login/autenticar",
-           data: { 'userName' : response.name, 'userPhoto' : 'http://graph.facebook.com/' + response.id + '/picture', 'userEmail' : response.email }
-         }).done(function(data) {
-         	if(data)
-         		window.location.href='http://localhost/dengue-em-foco/index.php/pages/view/mapa';
-         	else
-         		window.location.href='http://localhost/dengue-em-foco/index.php/pages/view/';
-         });
+      //Captura informações do PHP
+      $.ajax({
+        type: 'POST',
+        url: "http://localhost/dengue-em-foco/index.php/pages/login/autenticar",
+        data: { 'userName' : response.name, 'userPhoto' : 'http://graph.facebook.com/' + response.id + '/picture', 'userEmail' : response.email }
+      }).done(function(data) {
+        if (data) {
+          window.location.href='http://localhost/dengue-em-foco/index.php/pages/view/mapa';
+        } else {
+          window.location.href='http://localhost/dengue-em-foco/index.php/pages/view/';
+        }
+      });
     });
   }
-	function logout()
-	{
+
+	function logout () {
 		FB.logout(function(response) {
-				// Person is now logged out
-				
+			// Person is now logged out
+
 		});
 	}
