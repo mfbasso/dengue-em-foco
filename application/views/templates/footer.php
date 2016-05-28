ace885b8a31d0c5c0619340eb4225c4e4ba8b3a1
Isac Fadoni <isacfadoni@gmail.com>
2016-05-28 20:07:59 -0300

    Adicionado caminho para scripts minificado.

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,5 +1,5 @@
     <script src="<?php echo base_url(); ?>/assets/scripts/vendor/jquery-2.2.3.min.js"></script>
     <script src="https://apis.google.com/js/platform.js" async defer></script>
-    <script src="<?php echo base_url(); ?>/assets/scripts/config.js"></script>
+    <script src="<?php echo base_url(); ?>/assets/dist/main.min.js"></script>
   </body>
 </html>
