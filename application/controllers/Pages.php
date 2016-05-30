c6ab214f32f42589963930da9b0b734f313b14ee
Renan Luiz Vendramini <rlvendramini@outlook.com>
2016-05-30 07:49:08 -0300

    inclusão da futura página de cabeçalho

--- application/controllers/Pages.php
+++ application/controllers/Pages.php
@@ -4,17 +4,17 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Pages extends CI_Controller {
 	public function view($page = 'home')
 	{
-	        if ( ! file_exists(APPPATH.'views/pages/'.$page.'.php'))
-	        {
-             // Whoops, we don't have a page for that!
-             show_404();
-	        }
+     if ( ! file_exists(APPPATH.'views/pages/'.$page.'.php'))
+     {
+       // Whoops, we don't have a page for that!
+       show_404();
+     }
 
-	        $data['title'] = ucfirst($page); // Capitalize the first letter
+     $data['title'] = ucfirst($page); // Capitalize the first letter
 
-	      	//$this->load->view('templates/header', $data);
-	      	$this->load->view('pages/'.$page, $data);
-	      	$this->load->view('templates/footer', $data);
+   	$this->load->view('templates/header', $data);
+   	$this->load->view('pages/'.$page, $data);
+   	$this->load->view('templates/footer', $data);
 	}
 }
 ?>
